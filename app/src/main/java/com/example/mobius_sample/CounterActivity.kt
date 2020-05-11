356644f8e3b52033dc94256f2963e298e2024db6
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-11 19:45:37 +0530

    Hello, World!

---
+++ app/src/main/java/com/example/mobius_sample/CounterActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobius_sample
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CounterActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/example/mobius_sample/CounterFragment.kt
@@ -0,0 +1,5 @@
+package com.example.mobius_sample
+
+import androidx.fragment.app.Fragment
+
+class CounterFragment : Fragment(R.layout.fragment_counter)
