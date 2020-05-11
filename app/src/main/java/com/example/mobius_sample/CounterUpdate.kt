7804b009d2492c7bcd2049a9e91e1a59b88fd378
Sasikanth Miriyampalli <sasikanthmiriyampalli@gmail.com>
2020-05-11 20:25:59 +0530

    Do nothing when decremented value is below zero

--- app/src/main/java/com/example/mobius_sample/CounterUpdate.kt
+++ app/src/main/java/com/example/mobius_sample/CounterUpdate.kt
@@ -2,13 +2,23 @@ package com.example.mobius_sample
 
 import com.spotify.mobius.Next
 import com.spotify.mobius.Next.next
+import com.spotify.mobius.Next.noChange
 import com.spotify.mobius.Update
 
 class CounterUpdate : Update<CounterModel, CounterEvent, Nothing> {
     override fun update(model: CounterModel, event: CounterEvent): Next<CounterModel, Nothing> {
-        return when(event) {
+        return when (event) {
             Increment -> next(model.increment())
-            Decrement -> next(model.decrement())
+            Decrement -> decrementValue(model)
+        }
+    }
+
+    private fun decrementValue(model: CounterModel): Next<CounterModel, Nothing> {
+        val decrementedModel = model.decrement()
+        return if (decrementedModel.value < 0) {
+            noChange()
+        } else {
+            next(model.decrement())
         }
     }
 }
--- app/src/test/java/com/example/mobius_sample/CounterUpdateTest.kt
+++ app/src/test/java/com/example/mobius_sample/CounterUpdateTest.kt
@@ -1,7 +1,6 @@
 package com.example.mobius_sample
 
-import com.spotify.mobius.test.NextMatchers.hasModel
-import com.spotify.mobius.test.NextMatchers.hasNoEffects
+import com.spotify.mobius.test.NextMatchers.*
 import com.spotify.mobius.test.UpdateSpec
 import com.spotify.mobius.test.UpdateSpec.assertThatNext
 import org.junit.Test
@@ -26,14 +25,27 @@ class CounterUpdateTest {
 
     @Test
     fun `whenever decrement is clicked, then decrement the value`() {
+        val model = defaultModel.increment()
+
         updateSpec
-            .given(defaultModel)
+            .given(model)
             .whenEvent(Decrement)
             .then(
                 assertThatNext(
-                    hasModel(defaultModel.decrement()),
+                    hasModel(model.decrement()),
                     hasNoEffects()
                 )
             )
     }
+
+    @Test
+    fun `whenever counter value goes below zero , then do nothing`() {
+        updateSpec
+            .given(defaultModel)
+            .whenEvent(Decrement)
+            .then(
+                assertThatNext(hasNothing())
+            )
+    }
+
 }
